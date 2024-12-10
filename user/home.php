7f011f3eb01a42ece40784298b3b8d5f6422faeb
Nikesh Adhikari <nikeshad254@gmail.com>
2024-12-10 08:39:07 +0545

    html css for todo home created

--- user/home.php
+++ user/home.php
@@ -8,6 +8,24 @@ if (isset($_SESSION['role']) || $_SESSION['role'] != "user") {
 ?>
 
 
-<h1>user ko Home</h1>
+<link rel="stylesheet" href="../assets/css/todos.css">
+<div class="sectionContainer">
+    <h1>Todos</h1>
+    <form class="addForm" action="" method="post">
+        <input type="text" name="task" placeholder="Add Task ..." class="input">
+        <button class="btn" type="submit" name="addTodo">Add TODO</button>
+    </form>
+
+    <p class="todosContainerTitle">All Todos</p>
+    <ul class="todosContainer">
+        <li>
+            <form action="">
+                <input type="text" placeholder="Task...">
+                <button>📝</button>
+                <button>❌</button>
+            </form>
+        </li>
+    </ul>
+</div>
 
 <?php include("../inc/footer.php"); ?>
